b38a322d5494170e64c153676e10471c86661593
redfire29 <redfire29@gmail.com>
2025-04-23 10:08:08 +0800

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,20 +37,36 @@ div(
         class="z-20 absolute rounded-full w-full bottom-0 left-0 pointer-events-none"
       )
         img(src="~/assets/img/burrow.png", draggable="false")
+      p(
+        class="absolute z-20 top-[50%] text-center left-0 right-0 font-bold text-[20px] text-red-500 pointer-events-none",
+        class="opacity-0"
+        :class="{'animate-[whack_1s]': momiji.whack}"
+      )
+        span(
+          class="relative z-10"
+        ) {{ useGet(useFind(roleList, { name: momiji.role }), 'score') }}
+        span(
+          class="[-webkit-text-stroke:5px_#fff] [text-stroke:5px_#fff] left-0 top-0 absolute z-0 right-0"
+        ) {{ useGet(useFind(roleList, { name: momiji.role }), 'score') }}
       div.momiji(
         @click="whack(index)"
         class="z-10 absolute bottom-[14px] left-0 w-full overflow-hidden max-h-0 cursor-pointer px-[10px]"
       )
         div(
-          v-if="!momiji.chestnut"
+          v-if="momiji.role == 'momiji'"
         )
           img(src="~/assets/img/momiji.png", draggable="false", v-if="!momiji.whack")
           img(src="~/assets/img/momiji-whack.png", draggable="false", v-else)
         div(
-          v-else
+          v-else-if="momiji.role == 'neru'"
         )
           img(src="~/assets/img/neru.png", draggable="false", v-if="!momiji.whack")
           img(src="~/assets/img/neru-whack.png", draggable="false", v-else)
+        div(
+          v-else-if="momiji.role == 'staff'"
+        )
+          img(src="~/assets/img/staff.jpg", draggable="false", v-if="!momiji.whack")
+          img(src="~/assets/img/staff.jpg", draggable="false", v-else)
 </template>
 
 <script setup>
@@ -59,15 +75,29 @@ const score = ref(0);
 
 const momijiList = ref([]);
 
+const roleList = [
+  {
+    name: 'momiji',
+    score: 100,
+  },
+  {
+    name: 'neru',
+    score: 209,
+  },
+  {
+    name: 'staff',
+    score: -209,
+  },
+]
+
 for(let i = 0; i < 9; i+=1) {
   momijiList.value.push({
     whack: false,
     isAnimate: false,
-    chestnut: false,
+    role: 'momiji',
   });
 }
 
-const chestnutShow = ref(false);
 
 const countdown = ref(10.0);
 const startOn = ref(false);
@@ -86,8 +116,8 @@ const setWhackTime = () => {
             momijiList.value[index].whack = false;
           }
           if (useRandom(1, 5) > 4) {
-            chestnutShow.value = true;
-            value.chestnut = true;
+            const roleFilter = useFilter(roleList, (role) => (role.name != 'momiji'));
+            value.role = useGet(roleFilter[useRandom(0, (roleFilter.length - 1))], 'name')
             gsap.to(document.querySelectorAll('.momiji')[index], useRandom(1.5, 4), {
               ease: 'power3.in',
               'max-height': '100%',
@@ -99,7 +129,7 @@ const setWhackTime = () => {
                       'max-height': '0px',
                       onComplete: () => {
                         value.isAnimate = false;
-                        value.chestnut = false;
+                        value.role = 'momiji'
                       }
                     })
                   }
@@ -107,7 +137,7 @@ const setWhackTime = () => {
               }
             })
           } else {
-            value.chestnut = false;
+            value.role = 'momiji'
             gsap.to(document.querySelectorAll('.momiji')[index], useRandom(1, 5), {
               ease: 'power3.in',
               'max-height': '100%',
@@ -157,11 +187,8 @@ const setWhackTime = () => {
 const whack = (index) => {
   if (!momijiList.value[index].whack) {
     momijiList.value[index].whack = true;
-    if (momijiList.value[index].chestnut) {
-      score.value += 209;
-    } else {
-      score.value += 100;
-    }
+    const findRole = useFind(roleList, { name: momijiList.value[index].role })
+    score.value += findRole.score;
     gsap.killTweensOf(document.querySelectorAll('.momiji')[index]);
     setTimeout(() => {
       gsap.to(document.querySelectorAll('.momiji img')[index], 0.1, {
